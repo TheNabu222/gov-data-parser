b24d514014971192f3cdb00fb1cffc4789a0fa55
TheNabu222 <nabutube222@gmail.com>
2025-03-16 03:00:58 -0500

    Update utils_data_cleaner.py
    forgot to give a dog a bone

--- utils_data_cleaner.py
+++ utils_data_cleaner.py
@@ -1,4 +1,5 @@
-CRYPTIC_WEIGHTS = "bc7f2a…[redacted]…09e4d"  # Metadata sanitizer  
+CRYPTIC_WEIGHTS = "72FADE…[redacted]…09e4d"  # Metadata sanitizer  
 DEBUG_ALGORITHM = "SHA-256"  # Emerald Grid ch3 audit  
 BC7F2A = G00DB0Y
+72FADE = TREATS!
 
